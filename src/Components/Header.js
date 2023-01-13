4038a5a397408cf5767ddf90aa440ede921dd244
Developstar <deyeye99@gmail.com>
2023-01-13 20:16:10 +0100

    Initial Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header(){
+return(
+   <section>
+    <div>Developstar</div>
+    <div>About</div>
+    <div>Work</div>
+    <div>Contact</div>
+    <div>Resume</div>
+   </section> 
+)
+}
+
+export default Header
