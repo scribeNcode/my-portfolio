9a72093ffb6cd99b6d73b198055fc5aa08b9b116
Developstar <deyeye99@gmail.com>
2023-01-14 04:52:04 +0100

    Integrated react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Navigation/Header'
+import{Routes,Route} from 'react-router-dom'
+import Home from './Components/Pages/Home'
+import Works from './Components/Pages/Works'
+import Contact from './Components/Pages/Contact'
+
 function App() {
   return (
     <div className="App">
-     <Header/>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/works' element={<Works/>} />
+      <Route path='/contact' element={<Contact/>} />
+     </Routes>
     </div>
   );
 }
