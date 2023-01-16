8b210380a1eac211c57d43eb7f8b5a4bb147709c
Developstar <deyeye99@gmail.com>
2023-01-16 03:46:56 +0100

    added the header component into the home component

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Components/Navigation/Header'
+
+function Home(){
+    return(
+        <section>
+            <Header />
+            <h1>Home Page</h1>
+        </section>
+    )
+}
+
+export default Home;
