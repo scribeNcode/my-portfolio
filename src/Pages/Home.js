dc1b1557f2cd07afcd38b6d0bf15066cb06210f2
Developstar <deyeye99@gmail.com>
2023-01-16 04:21:19 +0100

    Introcductory text added to Intro div

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,14 @@ function Home(){
     return(
         <section>
             <Header />
-            <h1>Home Page</h1>
+            <section className='homePage'>
+                <div className='intro'>
+                    <p>Hi, my name is</p>
+                    <h1>Adeyeye Boluwatife</h1>
+                    <h1>I specialize in creating digital products and establishing brand identities.</h1>
+                    <p>As a software engineer, I specialize in developing high-quality and responsive mobile & web applications utilizing technologies such as HTML, CSS, JavaScript, ReactJS, and VueJS.</p>
+                </div>
+            </section>
         </section>
     )
 }
