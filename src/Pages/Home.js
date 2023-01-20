e12b21915eba7e67ed40d33f63c7fecb33453bfc
Developstar <deyeye99@gmail.com>
2023-01-20 14:18:31 +0100

    Added Image and Button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import Header from '../Components/Navigation/Header'
+import Button from '../Components/Navigation/Button'
+import image from '../Images/Oluwaseun.jpg'
 
 function Home(){
     return(
-        <section>
+        <section className='Home'>
             <Header />
-            <section className='homePage'>
+            <section className='HomePage'>
                 <div className='intro'>
                     <p>Hi, my name is</p>
                     <h1>Adeyeye Boluwatife</h1>
-                    <h1>I specialize in creating digital products and establishing brand identities.</h1>
+                    <h1>I build Interactive and Visually appealing web interfaces</h1>
                     <p>As a software engineer, I specialize in developing high-quality and responsive mobile & web applications utilizing technologies such as HTML, CSS, JavaScript, ReactJS, and VueJS.</p>
                 </div>
+                <div className='my_image'>
+                    <img src={image} alt='my'/>
+                </div>
             </section>
+            <div>
+                   <Button />
+                </div>
         </section>
     )
 }
