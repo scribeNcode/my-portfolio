0e2cf0ecf8177f12c79c32ef089eece5df49b2cd
Developstar <deyeye99@gmail.com>
2023-01-31 04:42:36 +0100

    home page styled

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -20,30 +20,11 @@ function Home() {
           <div className="my_image">
             <img src={image} alt="my" />
           </div>
-          
         </div>
         <div className="button">
-            <Button />
-          </div>
-      </section>
-
-      <section className="HomePage">
-        <div className="intro">
-          <h1>I build Interactive and Visually appealing web interfaces</h1>
-          <p>
-            As a software engineer, I specialize in developing high-quality and
-            responsive mobile & web applications utilizing technologies such as
-            HTML, CSS, JavaScript, ReactJS, and VueJS.
-          </p>
-          <div></div>
-        </div>
-        <div className="my_image">
-          <img src={image} alt="my" />
+          <Button />
         </div>
       </section>
-      <div className="button">
-        <Button />
-      </div>
     </section>
   );
 }
