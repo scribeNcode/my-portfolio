dbe9557eff57fda1c27ff0aa583e26234e832798
Developstar <deyeye99@gmail.com>
2023-02-02 07:48:23 +0100

    rearranged the header section

--- src/Components/Navigation/Header.js
+++ src/Components/Navigation/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import Button from './Button'
 function Header(){
 return(
    <section className='headerSection'>
-    <div className='logo'>Developstar</div>
-    <div className='navLinks'>
-      <li>About</li>
+    <div className='logo'>
+      <div>Dev</div>
+      <div className='navLinks'>
+         <li>About</li>
       <li>Work</li>
       <li>Contact</li>
+      </div>
+     
+    </div>
+    <div>
+      <Button />
     </div>
     
    </section> 
