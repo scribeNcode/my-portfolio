625af6ae8344f46804ae5f9503bd504a99be78df
Developstar <deyeye99@gmail.com>
2023-02-12 03:05:59 +0100

    Header, Herosec and footer components imported

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Footer from "../Components/Navigation/Footer";
+import Header from "../Components/Navigation/Header";
+import HeroImg from '../Components/Heroimage';
 
 function Contact(){
     return(
-        <section>
-            <h1>Contact Page</h1>
-        </section>
-    )
+        <div>
+         <Header />
+         <HeroImg heading="Contact Me" text="Discuss your project with me"/>
+         <Footer />
+        </div>
+     )
 }
 
 export default Contact;
