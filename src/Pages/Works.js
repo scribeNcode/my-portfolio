f035818021debbb4d7e71a5903bc38fa32f33522
Developstar <deyeye99@gmail.com>
2023-02-12 03:00:44 +0100

    Header, Herosec and footer components imported

--- src/Pages/Works.js
+++ src/Pages/Works.js
@@ -1,70 +1,17 @@
 
-import React,{useState, useEffect}from "react";
-function Works(){
-    //setup our project info state
-  const [currentItem, setCurrentItem] = useState(0)
-  const [project, setProject] = useState(null)
-  // const [loading, setLoading] = useState(null )
+import React from "react";
+import Footer from "../Components/Navigation/Footer";
+import Header from "../Components/Navigation/Header";
+import HeroImg from "../Components/Heroimage"
 
-  useEffect(()=>{
-    //list of items
-    const projectItems = [
-      {
-        title: 'Project 1',
-        about: "I built this project1 with"
-      },
-      {
-        title: 'project 2',
-        about: 'I built this project2 with'
-      },
-      {
-        title: 'project 3',
-        about: 'I built this project 3 with'
-      }
-    ];
-
-    const Item = projectItems[currentItem]
-    //set our state
-    setProject(Item)
-    // setLoading(false);
-  },[currentItem]);
-
-  //handle next button click
-  const handleNext = (event)=>{
-    event.preventDefault();
-    setCurrentItem(prev => prev + 1);
-  }
-  
-
-  //handle prev button click
-  const handlePrevious = (event)=>{
-    event.preventDefault();
-    setCurrentItem(prev => prev - 1);
-  }
-  
-
-    return(
-        <>
-        <section>
-            <h1>Works</h1>
-        </section>
-        <section className = 'works'>
-        <section className="news">
-          {
-            <div className="items">
-              <h1>{project.title}</h1>
-              <p>{project.about}</p>
-            </div>
-           }
-        </section>
-
-        <div>
-          <button onClick={handlePrevious}>Prev</button>
-          <button onClick={handleNext}>Prev</button>
-        </div>
-      </section>
-      </>
-    )
+function Works() {
+  return (
+    <div>
+      <Header />
+      <HeroImg heading="PROJECTS" text="My latest Projects" />
+      <Footer />
+    </div>
+  );
 }
 
 export default Works;
