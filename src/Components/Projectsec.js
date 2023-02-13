64306585ee05f36203a79e2a87c08c5880273b90
Developstar <deyeye99@gmail.com>
2023-02-13 04:16:19 +0100

    project component created

---
+++ src/Components/Projectsec.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Components/ProjectsecStyles.css"
+
+export default function Projectsec() {
+  return (
+    <div className='project_container'>
+        <h1 className='project_heading'>Projects</h1>
+        <div className='project_container'>
+            <div className='project-image'>
+                <img src='' alt='' />
+            </div>
+        </div>
+    </div>
+  )
+}
