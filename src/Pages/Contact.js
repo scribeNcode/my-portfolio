8c98d6d0215119c9c78ac3529af9a4c23d2d2862
Developstar <deyeye99@gmail.com>
2023-02-13 04:17:26 +0100

    imported signup component into my signup page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from "../Components/Navigation/Footer";
 import Header from "../Components/Navigation/Header";
 import HeroImg from '../Components/Heroimage';
+import SignUp from '../Components/SignUp';
 
 function Contact(){
     return(
         <div>
          <Header />
          <HeroImg heading="Contact Me" text="Discuss your project with me"/>
+         <SignUp/>
          <Footer />
         </div>
      )
