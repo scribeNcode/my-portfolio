236eff9ce7ad7c6cdf8d580b79cbbca7721e0085
Developstar <deyeye99@gmail.com>
2023-02-17 15:49:07 +0100

    Aboutme card componet edited

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,27 +1,24 @@
 import React from "react";
 import Header from "../Components/Navigation/Header";
-import Footer from "../Components/Navigation/Footer"
+import Footer from "../Components/Navigation/Footer";
 import image from "../Images/myImage.png";
 import techbro from "../Images/techbro.png";
-import AboutMeCard from "../Components/AboutMeCard"
+import AboutMeCard from "../Components/AboutMeCard";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaLeanpub } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
+import Technicalskills from "../Components/TechnicalSkillls/Technicalskills"
 
 function Home() {
-  
-
-  
   return (
     <section className="Home">
       <Header />
       <div className="HomePageWrapper">
         <div className="socialMediaIcons">
           <p>
-            <FaGithub  />
+            <FaGithub />
           </p>
           <p>
             <FaLinkedin />
@@ -36,56 +33,58 @@ function Home() {
         <section className="HomePage">
           <div className="home__display">
             <div className="intro">
-              <h1>I build Interactive and Visually appealing web interfaces</h1>
+              <h1>
+                Building Interactive and Visually appealing web interfaces
+              </h1>
               <p>
                 As a software engineer, I specialize in developing high-quality
                 and responsive mobile & web applications utilizing technologies
                 such as HTML, CSS, JavaScript, ReactJS, and VueJS.
-                <div className="button">
-            <Link to= "/project" className="btn">Projects</Link>
-            <Link to= "/contact" className="btn  btn-light">Contact</Link>
-          </div>
               </p>
+              <div className="button">
+                <Link to="/project" className="btn">
+                  Projects
+                </Link>
+                <Link to="/contact" className="btn  btn-light">
+                  Contact
+                </Link>
+              </div>
             </div>
             <div className="my_image">
               <img src={image} alt="my" />
             </div>
           </div>
-          
         </section>
       </div>
 
       <section className="abtMe_card">
         <div className="card1">
-          < AboutMeCard text="I am a frontend developer with over a year of experience in
-                developing user-facing features with ReactJs, VueJs, and
-                JavaScript. I am passionate about staying current in my field
-                and constantly learning new technologies and processes. I am a
-                quick learner, proactive in seeking growth opportunities, and
-                adaptable to industry changes. This mindset helps me to be
-                effective and stay ahead in my role."/>
+          <AboutMeCard
+            text="My name is Adeyeye Boluuwatife, "
+          />
         </div>
         <div className="card2">
-          < AboutMeCard/>
+          <AboutMeCard
+            text="Adeyeye Boluwatife"
+          />
         </div>
-      
-      </section>
-      <section className="abtMe_card2">
         <div className="card3">
-          < AboutMeCard/>
+          <AboutMeCard
+            text=""
+          />
         </div>
-        
       </section>
-      
-
-      
-
+      <div className="cvButton">
+          <Link to="/project" className="btn cvbtn">
+          Projects
+        </Link>
+      </div>
       <section>
-        
-        <Footer/>
+        <Technicalskills/>
       </section>
-      
-      
+      <section>
+        <Footer />
+      </section>
     </section>
   );
 }
