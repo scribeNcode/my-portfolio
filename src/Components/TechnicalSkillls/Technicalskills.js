5e32c29687d645d5bdbf491ca91ee26618b1d676
Developstar <deyeye99@gmail.com>
2023-02-17 17:40:19 +0100

    technical skills first row created

--- src/Components/TechnicalSkillls/Technicalskills.js
+++ src/Components/TechnicalSkillls/Technicalskills.js
@@ -1,23 +1,49 @@
 import React from 'react'
 import "../TechnicalSkillls/TechnicalStyles.css"
-import html from "../TechnicalSkillls/Images/Using/html.png.png"
+import html from "../TechnicalSkillls/Images/Using/html.png"
+import css  from "../TechnicalSkillls/Images/Using/css.png"
+import js from "../TechnicalSkillls/Images/Using/js.png.png"
+import react from "../TechnicalSkillls/Images/Using/react.png"
 
 export default function Technicalskills() {
   return (
-    <section className='technical_skills'>
+    <section className='skills_section'>
+      <h1>Technical Skills</h1>
+      <h5>Using</h5>
+      <section className='technical_skills'>
       <div className='row1'>
-        <div>
+        <div className='skillsdiv'>
           <img src={html} alt="html image" />
+          <div className='skilltext'>
+            <h4>HTML5</h4>
+          </div>
         </div>
-        <div>2</div>
+        <div className='skillsdiv big'>
+        <img src={css} alt="css image" />
+          <div className='skilltext'>
+            <h4>CSS3</h4>
+          </div>
+        </div>
       </div>
 
       <div className='row2'>
-        <div>1</div>
-        <div>2</div>
+        <div className='skillsdiv '>
+        <img src={js} alt="js image" />
+        <div className='skilltext'>
+            <h4>JavaScript</h4>
+          </div>
+        </div>
+        <div className='skillsdiv '>
+        <img src={react} alt="react" />
+        <div className='skilltext'>
+            <h4>React</h4>
+          </div>
+        </div>
       </div>
       
     </section>
+    </section>
+    
     
   )
 }
