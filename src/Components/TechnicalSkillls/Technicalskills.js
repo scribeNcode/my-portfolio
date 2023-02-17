584cca46a6050634ddd7340d63d65c5218e1705c
Developstar <deyeye99@gmail.com>
2023-02-17 15:52:58 +0100

    technical skills component created

---
+++ src/Components/TechnicalSkillls/Technicalskills.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../TechnicalSkillls/TechnicalStyles.css"
+
+export default function Technicalskills() {
+  return (
+    <section className='technical_skills'>
+      <div className='row1'>
+        <div>1</div>
+        <div>2</div>
+      </div>
+
+      <div className='row2'>
+        <div>1</div>
+        <div>2</div>
+      </div>
+      
+    </section>
+    
+  )
+}
