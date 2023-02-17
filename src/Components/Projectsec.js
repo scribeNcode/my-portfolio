47804aabed033781e885872d612ab66634601584
Developstar <deyeye99@gmail.com>
2023-02-17 15:52:34 +0100

    project page added

--- src/Components/Projectsec.js
+++ src/Components/Projectsec.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import "../Components/ProjectsecStyles.css"
+import Project1 from "./ProjectImage/Project1.png"
 
-export default function Projectsec() {
+export default function Projectsec(prop) {
   return (
-    <div className='project_container'>
+    <div className='projectPage_container'>
         <h1 className='project_heading'>Projects</h1>
         <div className='project_container'>
             <div className='project-image'>
-                <img src='' alt='' />
+                <img src={Project1} alt='' />
+                <h2 className='project-title'>{prop.projectTitle}</h2>
+                <div className='project_details'>
+                  <p>This is a text</p>
+                  <div className='pro-btns'>
+                    <NavLink to= "http://exam-gamma.vercel.app"
+                    className="btn">View</NavLink>
+                    <NavLink to= "http://exam-gamma.vercel.app"
+                    className="btn">Source</NavLink>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
