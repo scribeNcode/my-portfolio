7f2ac6bbe71de37f708313034fe7cc554c29f1bf
Developstar <deyeye99@gmail.com>
2023-02-17 16:11:25 +0100

    Technical skills images updated

--- src/Components/TechnicalSkillls/Technicalskills.js
+++ src/Components/TechnicalSkillls/Technicalskills.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import "../TechnicalSkillls/TechnicalStyles.css"
+import html from "../TechnicalSkillls/Images/Using/html.png.png"
 
 export default function Technicalskills() {
   return (
     <section className='technical_skills'>
       <div className='row1'>
-        <div>1</div>
+        <div>
+          <img src={html} alt="html image" />
+        </div>
         <div>2</div>
       </div>
 
