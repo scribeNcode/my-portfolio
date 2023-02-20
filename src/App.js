c93333d6b89f3c7511c276cf361cd46700c2d89e
Developstar <deyeye99@gmail.com>
2023-02-20 20:32:38 +0100

    home folder created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import{Routes,Route} from 'react-router-dom'
-import Home from './Pages/Home'
+import Home from './Pages/Home/Home'
 import Works from './Pages/Works'
 import Contact from './Pages/Contact'
 import About from './Pages/About'
