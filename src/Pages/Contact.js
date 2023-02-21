582098e697c00d833fea6cd440f8050e575b541c
Developstar <deyeye99@gmail.com>
2023-02-21 10:15:58 +0100

    footer folder imported

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Footer from "../Components/Navigation/Footer";
+import Footer from "../Components/Navigation/Footer/Footer";
 import Header from "../Components/Navigation/Header/Header";
 import HeroImg from '../Components/Heroimage';
 import SignUp from '../Components/SignUp';
