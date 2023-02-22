42640fdf3b0b0f3108c9c730c6a35f46210675c3
Developstar <deyeye99@gmail.com>
2023-02-22 12:35:45 +0100

    Signup Folder created

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from "../Components/Navigation/Footer/Footer";
 import Header from "../Components/Navigation/Header/Header";
 import HeroImg from '../Components/Heroimage';
-import SignUp from '../Components/SignUp';
+import SignUp from '../Components/SIgnUp/SignUp';
 
 function Contact(){
     return(
