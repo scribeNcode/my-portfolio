b9a941ec13d06080132b5e5b6d54ce82d56ee4ac
Developstar <deyeye99@gmail.com>
2023-02-22 12:36:18 +0100

    Hero Image Projects and text removed

--- src/Pages/Works.js
+++ src/Pages/Works.js
@@ -3,13 +3,13 @@ import React from "react";
 import Footer from "../Components/Navigation/Footer/Footer";
 import Header from "../Components/Navigation/Header/Header";
 import HeroImg from "../Components/Heroimage"
-import Projectsec from "../Components/Projectsec";
+import Projectsec from "../Components/Projects/Projectsec";
 
 function Works() {
   return (
     <div>
       <Header/>
-      <HeroImg heading="PROJECTS" text="My latest Projects" />
+      <HeroImg />
       <Projectsec />
       <Footer />
     </div>
