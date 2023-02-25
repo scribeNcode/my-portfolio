7d0306fc5082f2a67c9434600ce538153d12afce
Developstar <deyeye99@gmail.com>
2023-02-25 13:10:05 +0100

    Project page created

---
+++ src/Components/ProjectPageCompo/ProjectPage.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "../ProjectPageCompo/ProjectPageStyles.css"
+export default function ProjectPage() {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div
+      className="my-div"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      {hovered ? <div>hover</div> : <div>!hover</div>}
+    </div>
+  );
+}
